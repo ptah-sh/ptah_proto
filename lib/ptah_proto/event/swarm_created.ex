37efe74a714a65bc1038f0cccc2552809a5eb5f6
Bohdan Shulha <b.shulha@pm.me>
2024-05-31 00:47:22 +0200

    feat: improve naming

--- lib/ptah_proto/event/swarm_created.ex
+++ lib/ptah_proto/event/swarm_created.ex
@@ -1,4 +1,4 @@
-defmodule PtahProto.Event.SwarmCreated.DockerData do
+defmodule PtahProto.Event.SwarmCreated.Docker do
   @enforce_keys [:swarm_id]
 
   defstruct swarm_id: ""
@@ -9,12 +9,14 @@ defmodule PtahProto.Event.SwarmCreated.DockerData do
 end
 
 defmodule PtahProto.Event.SwarmCreated do
-  alias PtahProto.Event.SwarmCreated.DockerData
+  alias PtahProto.Event.SwarmCreated.Docker
+
+  @enforce_keys [:swarm_id, :docker]
 
   defstruct swarm_id: 0, docker: nil
 
   @type t :: %__MODULE__{
           swarm_id: integer(),
-          docker: DockerData.t()
+          docker: Docker.t()
         }
 end
