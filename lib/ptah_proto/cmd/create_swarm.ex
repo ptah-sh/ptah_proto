fac2338a1897833aab3443aa183d90b8f799f174
Bohdan Shulha <b.shulha@pm.me>
2024-05-31 00:21:38 +0200

    feat: add swarm_id to CreateSwarm

--- lib/ptah_proto/cmd/create_swarm.ex
+++ lib/ptah_proto/cmd/create_swarm.ex
@@ -1,5 +1,13 @@
+defmodule PtahProto.Cmd.CreateSwarm.Data do
+  defmodule CreateSwarmPtah do
+    @enforce_keys [:swarm_id]
+
+    defstruct swarm_id: 0
+  end
+end
+
 defmodule PtahProto.Cmd.CreateSwarm do
-  defstruct [
-    # No Arguments at the moment
-  ]
+  alias PtahProto.Cmd.CreateSwarm.Data.CreateSwarmPtah
+
+  defstruct ptah: %CreateSwarmPtah{swarm_id: 0}
 end
