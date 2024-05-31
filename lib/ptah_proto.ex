0e1555c1f1b6ac8e00d3edc100c0f8c2d767335a
Bohdan Shulha <b.shulha@pm.me>
2024-05-31 11:33:44 +0200

    feat: try define proto

--- lib/ptah_proto.ex
+++ lib/ptah_proto.ex
@@ -1,22 +1,33 @@
 defmodule PtahProto do
+  @callback handle_packet(any(), any()) :: {:noreply, any()}
+
   alias PtahProto.{Cmd, Event}
 
   def parse("cmd:create_swarm", payload), do: Cmd.CreateSwarm.parse(payload)
   def parse("event:swarm_created", payload), do: Event.SwarmCreated.parse(payload)
 
-  def handle_message(name, payload, socket) do
-    packet = __MODULE__.parse(name, payload)
+  defp handle_message() do
+    quote do
+      def handle_message(name, payload, socket) do
+        packet = PtahProto.parse(name, payload)
 
-    send(self(), {packet, socket})
+        handle_packet(packet, socket)
+      end
+    end
   end
 
   def __using__(:phx_channel) do
     quote do
       use Phoenix.Channel
+
+      @behaviour PtahProto
+
       @impl true
       def handle_in(name, payload, socket) do
-        PtahProto.handle_message(name, payload, socket)
+        handle_message(name, payload, socket)
       end
+
+      unquote(handle_message())
     end
   end
 
@@ -24,10 +35,14 @@ defmodule PtahProto do
     quote do
       use Slipstream
 
+      @behaviour PtahProto
+
       @impl Slipstream
       def handle_message(_topic, name, payload, socket) do
         PtahProto.handle_message(name, payload, socket)
       end
+
+      unquote(handle_message())
     end
   end
 end
