0abad5f4fb5acfa2e9eba82d82b8d15f2ef3862b
Bohdan Shulha <b.shulha@pm.me>
2024-06-03 17:39:47 +0200

    fix: try fix handle_message/4

--- lib/ptah_proto.ex
+++ lib/ptah_proto.ex
@@ -71,9 +71,7 @@ defmodule PtahProto do
 
       @impl Slipstream
       def handle_message(_topic, "ptah:" <> name, payload, socket) do
-        {:ok, _} = handle_message(name, payload, socket)
-
-        {:noreply, socket}
+        handle_message(name, payload, socket)
       end
 
       defp ptah_proto_push(socket, name, packet) do
