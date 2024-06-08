3fdf05e57744d1700e5b9b525145682c13ea86c3
Bohdan Shulha <b.shulha@pm.me>
2024-06-08 16:03:37 +0200

    feat: add config_id to create_config command

--- lib/ptah_proto/cmd/create_config.ex
+++ lib/ptah_proto/cmd/create_config.ex
@@ -1,15 +1,17 @@
 defmodule PtahProto.Cmd.CreateConfig do
   @derive Jason.Encoder
-  @enforce_keys [:name, :data]
-  defstruct name: "", data: %{}
+  @enforce_keys [:config_id, :name, :data]
+  defstruct config_id: 0, name: "", data: %{}
 
   @type t :: %__MODULE__{
+          config_id: integer(),
           name: String.t(),
           data: map()
         }
 
   def parse(%{} = payload) do
     %__MODULE__{
+      config_id: payload["config_id"],
       name: payload["name"],
       data: payload["data"]
     }
