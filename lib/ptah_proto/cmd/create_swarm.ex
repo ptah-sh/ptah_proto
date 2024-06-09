ec9c24d94e4bfbaf13f88c64c59a828a7a223a21
Bohdan Shulha <b.shulha@pm.me>
2024-06-09 18:05:06 +0200

    feat: add advertise_addr to the create_swarm command

--- lib/ptah_proto/cmd/create_swarm.ex
+++ lib/ptah_proto/cmd/create_swarm.ex
@@ -1,13 +1,14 @@
 defmodule PtahProto.Cmd.CreateSwarm do
-  @enforce_keys [:swarm_id]
+  @enforce_keys [:swarm_id, :advertise_addr]
   @derive Jason.Encoder
-  defstruct swarm_id: 0
+  defstruct swarm_id: 0, advertise_addr: ""
 
   @type t :: %__MODULE__{
-          swarm_id: integer()
+          swarm_id: integer(),
+          advertise_addr: String.t()
         }
 
   def parse(%{} = payload) do
-    %__MODULE__{swarm_id: payload["swarm_id"]}
+    %__MODULE__{swarm_id: payload["swarm_id"], advertise_addr: payload["advertise_addr"]}
   end
 end
