b54708b4f09c9cefbb4c2cea6b45fcb58e3ef59b
Bohdan Shulha <b.shulha@pm.me>
2024-06-09 19:20:06 +0200

    feat: add listen_addr to the create_swarm command

--- lib/ptah_proto/cmd/create_swarm.ex
+++ lib/ptah_proto/cmd/create_swarm.ex
@@ -1,14 +1,19 @@
 defmodule PtahProto.Cmd.CreateSwarm do
-  @enforce_keys [:swarm_id, :advertise_addr]
+  @enforce_keys [:swarm_id, :listen_addr, :advertise_addr]
   @derive Jason.Encoder
-  defstruct swarm_id: 0, advertise_addr: ""
+  defstruct swarm_id: 0, listen_addr: "", advertise_addr: ""
 
   @type t :: %__MODULE__{
           swarm_id: integer(),
+          listen_addr: String.t(),
           advertise_addr: String.t()
         }
 
   def parse(%{} = payload) do
-    %__MODULE__{swarm_id: payload["swarm_id"], advertise_addr: payload["advertise_addr"]}
+    %__MODULE__{
+      swarm_id: payload["swarm_id"],
+      listen_addr: payload["listen_addr"],
+      advertise_addr: payload["advertise_addr"]
+    }
   end
 end
